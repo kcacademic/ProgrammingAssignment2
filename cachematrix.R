396f36df017b3f019795256c82cf2c7da90d5511
Kumar Chandrakant <kchandrakant@sapient.com>
2015-09-16 23:13:36 +0530

    Creating the commit with completed script for the programming assignment for caching of matrix inversion.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The file cachematrix contains method to create inverse of 
+## matrix and cache the result for further calls.
 
-## Write a short comment describing this function
-
+## The function makeCacheMatrix caches a matrix and its
+## inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## The fucntion cacheSolve creates inverse of a matrix
+## if the same is not present in the cache.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data)
+  x$setinverse(m)
+  m
 }
